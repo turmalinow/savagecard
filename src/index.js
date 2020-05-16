d7c8dfed768b3e4cf8c763b68a69db9c59033e7f
Artur Malinowski <turmalinow@gmail.com>
2020-05-16 12:43:12 +0200

    clean start

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <h1>Savage Card JS</h1>;
+};
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+);
