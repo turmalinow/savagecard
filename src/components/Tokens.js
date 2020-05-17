90973dc6674058fc1c4e0a3ea0bbae0c82815d25
Artur Malinowski <turmalinow@gmail.com>
2020-05-17 21:04:52 +0200

    editable secondaries

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -4,9 +4,9 @@ import './Tokens.css';
 const Tokens = () => {
     return <div className="tokens">
         <div className="secondaries">
-            <i className="pace">6</i>
-            <i className="parry">5</i>
-            <i className="toughness">9</i>
+            <i className="pace double"><input value={'6"'}/><input value="k6"/></i>
+            <i className="parry"><input value="2"/></i>
+            <i className="toughness"><input value="4"/></i>
         </div>
         <div className="wounds">
             <i className="fatigue-1" />
