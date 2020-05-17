92ab8b9e5e0db958986f5b5f4e1514241567dfda
Artur Malinowski <turmalinow@gmail.com>
2020-05-17 16:16:50 +0200

    Updates

---
+++ precache-manifest.b38e7deaa948929ac49a4eaad0cf2e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a79dce277b2708794a046fbffd7734",
+    "url": "/savagecard/index.html"
+  },
+  {
+    "revision": "b9c1b638fe28f11eb1dc",
+    "url": "/savagecard/static/css/main.17ab7eb9.chunk.css"
+  },
+  {
+    "revision": "2476555e6a15e576a53f",
+    "url": "/savagecard/static/js/2.bf9aa0e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/savagecard/static/js/2.bf9aa0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c1b638fe28f11eb1dc",
+    "url": "/savagecard/static/js/main.13ec0aec.chunk.js"
+  },
+  {
+    "revision": "0b511a869a2e345aba44",
+    "url": "/savagecard/static/js/runtime-main.6d0594e2.js"
+  }
+]);
